4a39c0b4e2f9b6a04fbf29fa890bab6333a3eca0
zwkkk <45117316+zwkkk@users.noreply.github.com>
2019-11-02 00:47:37 +0800

    Update main.py

--- tutorials/02-intermediate/deep_residual_network/main.py
+++ tutorials/02-intermediate/deep_residual_network/main.py
@@ -47,16 +47,16 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 # 3x3 convolution
 def conv3x3(in_channels, out_channels, stride=1):
     return nn.Conv2d(in_channels, out_channels, kernel_size=3, 
-                     stride=stride, padding=1, bias=False)
+                     stride=stride, padding=1, bias=False) # 输出图片宽30。（30-3+1*2）+1 = 30 即输入输出图片尺寸不变
 
-# Residual block
+# Residual block  每一个残差模块
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(ResidualBlock, self).__init__()
-        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.conv1 = conv3x3(in_channels, out_channels, stride) 
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(out_channels, out_channels)
+        self.conv2 = conv3x3(out_channels, out_channels) 
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.downsample = downsample
         
@@ -69,7 +69,8 @@ class ResidualBlock(nn.Module):
         out = self.bn2(out)
         if self.downsample:
             residual = self.downsample(x)
-        out += residual
+           
+        out += residual  #此处为out和x张量相加，对应残差网络残差相加那一步。注：out和x张量维度一致
         out = self.relu(out)
         return out
 
@@ -78,10 +79,12 @@ class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes=10):
         super(ResNet, self).__init__()
         self.in_channels = 16
-        self.conv = conv3x3(3, 16)
+        self.conv = conv3x3(3, 16)   #图片宽30，16通道
         self.bn = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
-        self.layer1 = self.make_layer(block, 16, layers[0])
+        self.layer1 = self.make_layer(block, 16, layers[0]) #输入参数：1.引入残差模块 2.输出通道数 
+                                                            #3.引入几个残差模块 
+                                                            #4.步长，即残差模块第一个卷积块的步长，若不为1，则输出尺寸变小，实现下采样      
         self.layer2 = self.make_layer(block, 32, layers[1], 2)
         self.layer3 = self.make_layer(block, 64, layers[2], 2)
         self.avg_pool = nn.AvgPool2d(8)
@@ -89,14 +92,14 @@ class ResNet(nn.Module):
         
     def make_layer(self, block, out_channels, blocks, stride=1):
         downsample = None
-        if (stride != 1) or (self.in_channels != out_channels):
+        if (stride != 1) or (self.in_channels != out_channels):   #步长若不为1 下采样
             downsample = nn.Sequential(
                 conv3x3(self.in_channels, out_channels, stride=stride),
                 nn.BatchNorm2d(out_channels))
         layers = []
         layers.append(block(self.in_channels, out_channels, stride, downsample))
         self.in_channels = out_channels
-        for i in range(1, blocks):
+        for i in range(1, blocks):   #几个残差块
             layers.append(block(out_channels, out_channels))
         return nn.Sequential(*layers)
     
@@ -108,7 +111,7 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.avg_pool(out)
-        out = out.view(out.size(0), -1)
+        out = out.view(out.size(0), -1) #将tensor拉平，out.size(0)为batch_size，第二维即为展平后的长度
         out = self.fc(out)
         return out
     
